ee0c776b471697934f6a927414e944b5e02b84d3
Elias Abreu <stevenabreu777@gmail.com>
2025-03-03 21:42:54 -0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const navMobile = document.querySelector(".nav-mobile");
+  menuToggle.addEventListener("click", function () {
+      navMobile.classList.toggle("ativo");
+  });
+});
